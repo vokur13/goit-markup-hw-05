435ca90eb34e3d77684fa522aed0b2444da8485a
vokur13 <vokur13@gmail.com>
2022-05-24 17:33:04 +0300

    js fixed

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,43 @@
+// Header positioning
+
+const { height: pageHeaderHeight } = document.querySelector('.header').getBoundingClientRect();
+
+document.body.style.paddingTop = `${pageHeaderHeight}px`;
+
 // Dropbox open-close
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
+
+// Dropbox Modal v. 2.0
+
+const refs = {
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+refs.backdrop.addEventListener('click', logBackdropClick);
+
+function toggleModal() {
+  refs.backdrop.classList.toggle('is-hidden');
+}
+
+function logBackdropClick() {
+  console.log('Это клик в бекдроп');
+}
